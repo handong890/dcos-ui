eecc62251b5ff029c089b220aae696e6bce482d9
Ioannis Charalampidis <johnys2@gmail.com>
2017-03-22 11:24:21 -0700

    Adding new version of integration test config script

--- .systemtest-dev.sh
+++ .systemtest-dev.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# System test configuration for running tests locally
+#
+# This is a configuration script to the system-test-driver that runs the
+# integration tests against the development version of the DC/OS UI
+#
 
-# Config
+# Require a cluster to be specified from the command-line
 CLUSTER_URL="$1"
+CLUSTER_AUTH_SCRIPT="$2"
 LOCAL_PORT=8050
 
-# Require a cluster
+# Validate
 if [ -z "$CLUSTER_URL" ]; then
   echo "Please specify the cluster URL to use"
   exit 1
 fi
 
+# Start an http proxy that will serve the built version of the DC/OS UI
+# against the cluster specified.
 cat <<EOF
 {
-
   "criteria": [],
 
   "suites": [
@@ -37,7 +42,7 @@ cat <<EOF
         "url": "http://127.0.0.1:$LOCAL_PORT"
       },
       "scripts": {
-        "auth": "./system-tests/_scripts/auth-ee.py"
+        "auth": "./system-tests/_scripts/auth-open.py"
       }
     }
   ]
